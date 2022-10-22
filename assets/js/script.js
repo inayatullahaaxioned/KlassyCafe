f100d4d3a76d6332ce1c9a18e3b06b62e6c255ce
inayatullahaaxioned <inayatullaha@axioned.com>
2022-10-22 18:45:19 +0530

    hamburger js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 /* Author: 
-
+Inayatullah
 */
+const hamburger = document.querySelector(".hamburger"),
+            nav = document.querySelector(".nav");
 
+            hamburger.addEventListener("click",function () {
+                hamburger.classList.toggle("active");
+                nav.classList.toggle("active");
+            });
 
 
 
